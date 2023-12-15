445279df9a09e4808b48e0ce3fed83a189c5f9d9
Felipe Costa <felipe.costa@synvia.com>
2023-12-15 14:58:43 -0300

    feat: add imgs and packages files and config tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,26 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
+  mode: "jit", // Habilita o modo JIT para Tailwind CSS
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/**/*.{js,ts,jsx,tsx}", // Ajuste os caminhos se necessário
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "custom-blue": "#197BBD",
       },
+      spacing: {
+        // Adicione aqui valores personalizados de espaçamento, se necessário
+      },
+      boxShadow: {
+        custom:
+          "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -2px rgba(0, 0, 0, 0.1)",
+      },
+      // Aqui você pode estender outras propriedades conforme necessário
     },
   },
   plugins: [],
-}
-export default config
+};
+
+export default config;
